86d32302530e66401f9e8435e94cb76e1066a7c5
Thomas Salmon <th_s@hotmail.fr>
2013-05-18 18:33:59 +0200

    in

--- interpreter.ml
+++ interpreter.ml
@@ -7,11 +7,9 @@ type value = Primitive.t Runtime.value
 
 type env = Primitive.t Runtime.venv
 
-
-let e = Env.empty () ;;
+let inter_vdefinition  = ""
 
 let program : AST.program -> env = function
-  | (DVal e)::b -> Env.empty ()
+  | (DVal a)::b -> Env.empty ()
   | a::b -> failwith "autre a::b"
   | _ -> failwith "non reconnu"
-
