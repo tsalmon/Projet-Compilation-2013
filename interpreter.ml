75d797b168284b1247ba8a2bc3f6ac5d10ea1450
beziau paul <bibibobe@ubuntu.(none)>
2013-05-18 20:41:02 +0200

    avec recursion sur plusieur ligne

--- interpreter.ml
+++ interpreter.ml
@@ -8,18 +8,25 @@ type value = Primitive.t Runtime.value
 type env = Primitive.t Runtime.venv
 
 
-let e = Env.empty () ;;
+let ea = Env.empty () ;;
 
-let vdefinition v  = function
-  | Simple (binding,expr) -> let eA=(Env.declare (AST.Named(AST.Identifier "x"))  e) in (Env.define (AST.Named(AST.Identifier "x")) (Runtime.VInt 3) eA);eA
- (* | MutuallyRecursive (binding, expr):: -> e'*)
-  |  _ -> failwith "vedefinition error"
 
+
+
+let rec programm e : AST.program -> env  =function 
+  | (DVal v)::b -> begin match v with
+      | Simple (Binding(a_i,_),expr) -> let eA=(Env.declare (a_i)  e) in (Env.define (a_i)  (Runtime.VInt 3) eA); programm eA b
+      end ; 
+  | [] -> e
+  | _ -> failwith "non reconnu"
 
 let program : AST.program -> env = function
+  | a -> programm ea a
+(*
   | (DVal v)::b -> begin match v with
       | Simple (Binding(a_i,_),expr) -> let eA=(Env.declare (a_i)  e) in (Env.define (a_i)  (Runtime.VInt 3) eA);eA
-      end ;
-  | a::b -> failwith "autre a::b"
-  | _ -> failwith "non reconnu"
+      end ; program b
+  | [] -> e
+  | _ -> failwith "non reconnu"*)
+
 
