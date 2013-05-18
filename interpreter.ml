38b8f75093d9056c8bef1296804418da9d44f0cd
beziau paul <bibibobe@ubuntu.(none)>
2013-05-18 18:32:46 +0200

    semi-victoire

--- interpreter.ml
+++ interpreter.ml
@@ -11,7 +11,7 @@ type env = Primitive.t Runtime.venv
 let e = Env.empty () ;;
 
 let program : AST.program -> env = function
-  | (DVal e)::b -> Env.empty ()
+  | (DVal v)::b ->  let eA=(Env.declare (AST.Named(AST.Identifier "x"))  e) in (Env.define (AST.Named(AST.Identifier "x")) (Runtime.VInt 3) eA);eA
   | a::b -> failwith "autre a::b"
   | _ -> failwith "non reconnu"
 
