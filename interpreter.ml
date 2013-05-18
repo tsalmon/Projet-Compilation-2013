5923dae8f0e14d9453d8fc4627a05cb2254b3c25
beziau paul <bibibobe@ubuntu.(none)>
2013-05-18 20:21:34 +0200

    avec nom de variable le 'let vdefinition' ne sert a rien

--- interpreter.ml
+++ interpreter.ml
@@ -10,8 +10,16 @@ type env = Primitive.t Runtime.venv
 
 let e = Env.empty () ;;
 
+let vdefinition v  = function
+  | Simple (binding,expr) -> let eA=(Env.declare (AST.Named(AST.Identifier "x"))  e) in (Env.define (AST.Named(AST.Identifier "x")) (Runtime.VInt 3) eA);eA
+ (* | MutuallyRecursive (binding, expr):: -> e'*)
+  |  _ -> failwith "vedefinition error"
+
+
 let program : AST.program -> env = function
-  | (DVal v)::b ->  let eA=(Env.declare (AST.Named(AST.Identifier "x"))  e) in (Env.define (AST.Named(AST.Identifier "x")) (Runtime.VInt 3) eA);eA
+  | (DVal v)::b -> begin match v with
+      | Simple (Binding(a_i,_),expr) -> let eA=(Env.declare (a_i)  e) in (Env.define (a_i)  (Runtime.VInt 3) eA);eA
+      end ;
   | a::b -> failwith "autre a::b"
   | _ -> failwith "non reconnu"
 
