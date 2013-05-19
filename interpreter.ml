fe9a4333dc1b6b82413bda61bd3ee1b6d5657039
beziau paul <bibibobe@ubuntu.(none)>
2013-05-19 10:09:07 +0200

    reference sur l'environment

--- interpreter.ml
+++ interpreter.ml
@@ -8,25 +8,34 @@ type value = Primitive.t Runtime.value
 type env = Primitive.t Runtime.venv
 
 
-let ea = Env.empty () ;;
+let e = ref (Env.empty ()) ;;
 
 
 
 
+let rec program: AST.program -> env  =function 
+  | (DVal v)::b -> begin match v with
+      | Simple (Binding(a_i,_),expr) ->  e:=(Env.declare (a_i)  !e) ; (Env.define (a_i)  (Runtime.VInt 3) !e); program b;
+      end ; 
+  | [] -> !e
+  | _ -> failwith "non reconnu"
+
+(*
 let rec programm e : AST.program -> env  =function 
   | (DVal v)::b -> begin match v with
-      | Simple (Binding(a_i,_),expr) -> let eA=(Env.declare (a_i)  e) in (Env.define (a_i)  (Runtime.VInt 3) eA); programm eA b
+      | Simple (Binding(a_i,_),expr) -> let eA=(Env.declare (a_i)  e) in (Env.define (a_i)  (Runtime.VInt 3) eA); programm eA b;
       end ; 
   | [] -> e
   | _ -> failwith "non reconnu"
 
 let program : AST.program -> env = function
-  | a -> programm ea a
-(*
+  | a -> let eA=(Env.declare (AST.Named(AST.Identifier "f"))  ea) in (Env.define (AST.Named(AST.Identifier "f"))  (Runtime.VInt 15) eA); programm eA a;
+
   | (DVal v)::b -> begin match v with
       | Simple (Binding(a_i,_),expr) -> let eA=(Env.declare (a_i)  e) in (Env.define (a_i)  (Runtime.VInt 3) eA);eA
       end ; program b
   | [] -> e
-  | _ -> failwith "non reconnu"*)
+  | _ -> failwith "non reconnu"
+*)
 
 
