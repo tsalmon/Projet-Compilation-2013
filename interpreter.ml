27420111d6d4e12a96a130eaa4b504f777a8989c
Thomas Salmon <th_s@hotmail.fr>
2013-05-19 12:14:50 +0200

    pedale dans le semoule

--- interpreter.ml
+++ interpreter.ml
@@ -12,12 +12,12 @@ type env = Primitive.t Runtime.venv
 (*
   | (DVal v)::b ->  
     let e=(Env.declare (AST.Named(AST.Identifier "x"))  e) in 
-    (Env.define (AST.Named(AST.Identifier "x")) (Runtime.VInt 30) e);
-    e
+  (Env.define (AST.Named(AST.Identifier "x")) (Runtime.VInt 30) e);
+  e
 *)
-
+    
 (* call by ipr_vdef *)
-let ipr_expr = function
+let ipr_expr ev = function
   | EInt(i) -> print_int(i);print_string("\n");failwith "int"
   | EChar(c) -> failwith "char"
   | EString(s) -> failwith "string"
@@ -31,20 +31,32 @@ let ipr_expr = function
   | ECase(t, b) -> failwith "case"
   | EFun( b, e) -> failwith "fun"
 
+let ipr_arg_id e = function
+  | Named(Identifier(s)) -> s
+  | Unnamed -> "_"
+
+let ipr_binding = function 
+  | Binding(ar, t) -> ipr_arg_id ar 
+
+
 (* call by ipr_def *)
-let ipr_vdef = function
-  | Simple (Binding(a,t), e) -> ipr_expr e
+let ipr_vdef e = function
+  | Simple (Binding(ar,t), EInt(i)) ->   
+    let x=(Env.declare (AST.Named(AST.Identifier "x"))  e) in 
+    (Env.define ar (Runtime.VInt i) x)
+  | Simple (b, exp) -> failwith "simple"
   | MutuallyRecursive m -> failwith "mutually"
 
 (* call by ipr_program *)
 let ipr_def  e = function 
   | DType (identifier, identifiers, t) -> failwith "dtype"
-  | DVal  v -> ipr_vdef v
-
+  | DVal  v -> ipr_vdef e v
+    
 (* call by program *)
 let rec ipr_program e = function
   | [] -> e
   | a::b -> ipr_program (ipr_def e a) b
-
+    
 let program : AST.program -> env = function
   | a -> ipr_program (Env.empty ()) a
+    
