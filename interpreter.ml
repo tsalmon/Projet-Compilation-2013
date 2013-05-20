9e8ba4dca39d22e70807f6ea5a5d36345505e265
beziau paul <bibibobe@ubuntu.(none)>
2013-05-20 11:33:34 +0200

    les fonction marche

--- interpreter.ml
+++ interpreter.ml
@@ -12,27 +12,33 @@ let e = ref (Env.empty ()) ;;
 
              
 
-let rec expression_app= function 
-    | (VPrimitive p,VInt i) -> Primitive.apply p (Runtime.VInt i)
-    | _ -> failwith "app non reconu"
-
-let rec expression = function 
+let rec expression env= function 
    | EInt i             -> Runtime.VInt i
    | EChar c            -> Runtime.VChar c
    | EString chaine     -> Runtime.VString chaine
-   | EVar id            -> if (Primitive.identifier id) then (Primitive.lookup id) else (Env.lookup (AST.Named id) !e)
+   | EVar id            -> if (Primitive.identifier id) then (Primitive.lookup id) else (Env.lookup (AST.Named id) env)
    | ESum (_,_,_)       -> failwith "expr non fonctionnel"
    | EProd (_,_)        -> failwith "expr non fonctionnel"
    | EAnnot (_,_)       -> failwith "expr non fonctionnel"
-   | ESeq (_)         -> failwith "expr non fonctionnel"
+   | ESeq (_)           -> failwith "expr non fonctionnel"
    | EDef (_,_)         -> failwith "expr non fonctionnel"
-   | EApp(exprA,exprB) ->  (expression_app ((expression exprA),(expression exprB)))
+   | EApp(exprA,exprB)  ->  (expression_app ((expression env exprA),(expression env exprB)))
    | ECase(_,_)         -> failwith "expr non fonctionnel"
-   | EFun (_,_)         -> failwith "expr non fonctionnel"
+   | EFun (b,f) -> VClosure((Env.empty ()), Branch(POne, EFun(b,f))::[])
+
+
+and expression_app = function 
+   | (VPrimitive p, i) -> Primitive.apply p  i
+   | ( (  VClosure (env,Branch(POne , EFun(Binding(a_i,_),EFun(b,f)))::[] )  )  , i ) ->  VClosure((Env.bind a_i i env), Branch(POne, EFun(b,f))::[])
+   | ( (  VClosure (env,Branch(POne , EFun(Binding(a_i,_),expr))::[] )  )  , i ) ->  expression (Env.bind a_i i env) expr
+   | _ -> failwith "app non reconu"
+
+
+
 
 let rec program: AST.program -> env  = function 
   | (DVal v)::b -> begin match v with
-      | Simple (Binding(a_i,_),expr) ->  e:=(Env.declare (a_i)  !e) ; (Env.define (a_i)  (expression expr)  !e); program b;
+      | Simple (Binding(a_i,_),expr) ->  e:=(Env.declare (a_i)  !e) ; (Env.define (a_i)  (expression !e expr)  !e); program b;
       end ; 
   | [] -> !e
   | _ -> failwith "non reconnu"
